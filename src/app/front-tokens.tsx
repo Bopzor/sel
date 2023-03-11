abbc89a79fba35f946a1511552603709644d37f3
nilscox <nilscox.dev@gmail.com>
2023-03-11 19:54:04 +0100

    fix: use global container and token instances

--- src/app/front-tokens.tsx
+++ src/app/front-tokens.tsx
@@ -1,15 +1,25 @@
-import { token, Container } from 'brandi';
+import { Container, token } from 'brandi';
 import { ContainerProvider as BrandiContainerProvider } from 'brandi-react';
 
 import { MembersService } from '../modules/members/members.service';
 import { RequestsService } from '../modules/requests/requests.service';
 
-export const FRONT_TOKENS = {
+/* eslint-disable no-var */
+declare global {
+  var FRONT_TOKENS: ReturnType<typeof createTokens>;
+  var container: Container;
+}
+
+const createTokens = () => ({
   membersService: token<MembersService>('membersService'),
   requestsService: token<RequestsService>('requestsService'),
-};
+});
 
-export const container = new Container();
+export const FRONT_TOKENS = globalThis.FRONT_TOKENS ?? createTokens();
+globalThis.FRONT_TOKENS ??= FRONT_TOKENS;
+
+export const container = globalThis.container ?? new Container();
+globalThis.container ??= container;
 
 container.bind(FRONT_TOKENS.membersService).toInstance(MembersService).inSingletonScope();
 container.bind(FRONT_TOKENS.requestsService).toInstance(RequestsService).inSingletonScope();
