7facba6548f23d24f8de3c09eeca3c2152042de8
nilscox <nilscox.dev@gmail.com>
2023-03-19 23:03:46 +0100

    chore: fixes according to review feedback

--- src/pages/events/events.page.tsx
+++ src/pages/events/events.page.tsx
@@ -3,11 +3,11 @@ import { BackLink } from '../../app/components/back-link';
 import calendarImage from './calendar.png';
 
 export const Page = () => (
-  <div className="col gap-4">
+  <>
     <BackLink href="/" />
 
     <div className="card mx-auto max-w-[56rem] overflow-hidden">
       <img src={calendarImage} alt="placeholder calendar" />
     </div>
-  </div>
+  </>
 );
