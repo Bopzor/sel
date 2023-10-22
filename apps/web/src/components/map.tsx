fc603db37796325cd5cdb3a360b5f54dca732dd7
nilscox <nils@koyeb.com>
2023-10-22 13:19:31 +0200

    feat(members): remove map placeholder

--- apps/web/src/components/map.tsx
+++ apps/web/src/components/map.tsx
@@ -20,14 +20,6 @@ type MapProps = {
 };
 
 export const Map: Component<MapProps> = (props) => {
-  return (
-    <div class={clsx(props.class, 'col items-center justify-center bg-black/5 text-lg text-neutral/50')}>
-      Carte
-    </div>
-  );
-};
-
-export const Map_: Component<MapProps> = (props) => {
   const id = createId();
 
   const [viewport, setViewport] = createSignal<Viewport>({
