e22aae0c5dfea2adf92126b64c66636480f807b1
nilscox <nils@koyeb.com>
2023-11-12 16:26:41 +0100

    infra(server): update domain event log format

--- apps/server/src/infrastructure/events/events-logger.ts
+++ apps/server/src/infrastructure/events/events-logger.ts
@@ -2,20 +2,28 @@ import { injectableClass } from 'ditox';
 
 import { DomainEvent } from '../../domain-event';
 import { TOKENS } from '../../tokens';
+import { DatePort } from '../date/date.port';
 import { LoggerPort } from '../logger/logger.port';
 
 import { EventsPort } from './events.port';
 
 export class EventsLogger {
-  static inject = injectableClass(this, TOKENS.events, TOKENS.logger);
+  static inject = injectableClass(this, TOKENS.date, TOKENS.events, TOKENS.logger);
 
-  constructor(private readonly events: EventsPort, private readonly logger: LoggerPort) {}
+  constructor(
+    private readonly dateAdapter: DatePort,
+    private readonly events: EventsPort,
+    private readonly logger: LoggerPort
+  ) {}
 
   init() {
     this.events.addAnyEventListener(this.logEvent.bind(this));
   }
 
   async logEvent(event: DomainEvent) {
-    this.logger.info(event);
+    const now = this.dateAdapter.now().toISOString();
+    const { type, entity, entityId, ...payload } = event;
+
+    this.logger.info(`[${now}] [${entity}]`, type, entityId, JSON.stringify(payload));
   }
 }
