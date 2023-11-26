23bba58b47f5a819f4eaf18760cec50a9f943a6b
nilscox <nils@koyeb.com>
2023-11-26 16:58:47 +0100

    feat(web): button disabled state

--- apps/web/src/components/button.tsx
+++ apps/web/src/components/button.tsx
@@ -9,23 +9,23 @@ type ButtonProps = ComponentProps<'button'> & {
   loading?: boolean;
 };
 
-export const Button: Component<ButtonProps> = (_props) => {
-  const props2 = mergeProps({ variant: 'primary' } satisfies ButtonProps, _props);
-  const [own, props] = splitProps(props2, ['variant', 'loading', 'classList']);
+export const Button: Component<ButtonProps> = (props1) => {
+  const props2 = mergeProps({ type: 'button', variant: 'primary' } satisfies ButtonProps, props1);
+  const [props, buttonProps] = splitProps(props2, ['variant', 'loading', 'disabled', 'classList']);
 
   return (
     <button
-      type="button"
+      disabled={props.disabled ?? props.loading}
       classList={{
         button: true,
-        'button-primary': own.variant === 'primary',
-        'button-secondary': own.variant === 'secondary',
-        ...own.classList,
+        'button-primary': props.variant === 'primary',
+        'button-secondary': props.variant === 'secondary',
+        ...props.classList,
       }}
-      {...props}
+      {...buttonProps}
     >
-      {props.children}
-      {own.loading && <Spinner class="h-1 w-1" />}
+      {buttonProps.children}
+      {props.loading && <Spinner class="h-4 w-4" />}
     </button>
   );
 };
@@ -38,7 +38,7 @@ export const LinkButton: Component<LinkButtonProps> = (props) => {
   return (
     <Link {...props} class={clsx('button', props.class)}>
       {props.children}
-      {props.loading && <Spinner class="h-1 w-1" />}
+      {props.loading && <Spinner class="h-4 w-4" />}
     </Link>
   );
 };
