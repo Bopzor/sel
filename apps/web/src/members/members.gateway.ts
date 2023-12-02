0c7bd2486f94bd9ef3ad22a10ff321e783bd2e3f
nilscox <nils@koyeb.com>
2023-12-02 13:50:57 +0100

    chore(web): remove unused code

--- apps/web/src/members/members.gateway.ts
+++
@@ -1,6 +0,0 @@
-import { Member, MembersSort } from '@sel/shared';
-
-export interface MembersGateway {
-  listMembers(sort?: MembersSort): Promise<Member[]>;
-  getMember(memberId: string): Promise<Member | undefined>;
-}
