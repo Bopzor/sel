1f0651a5fa2f1f78d1dc0dd9f0eec713a6be0419
nilscox <nils@koyeb.com>
2023-12-25 14:03:35 +0100

    chore: fix sql request file name

--- apps/server/src/requests/sql-request.repository.intg-spec.ts
+++ apps/server/src/requests/sql-request.repository.intg-spec.ts
@@ -29,7 +29,7 @@ class Test extends RepositoryTest {
   }
 }
 
-describe('SqlRequestRepository', () => {
+describe('[Intg] SqlRequestRepository', () => {
   let test: Test;
 
   beforeEach(async () => {
