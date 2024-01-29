355f19634e5a5b6ef52c354a68ea9f3589d3854d
nilscox <nils@koyeb.com>
2024-01-29 21:57:39 +0100

    chore(email): add a script to render emails

---
+++ packages/emails/render.js
@@ -0,0 +1,68 @@
+import fs from 'node:fs/promises';
+
+import emails from './dist/index.js';
+
+function output({ subject, text, html }) {
+  return [
+    //
+    `<h1>${subject}</h1>`,
+    `<pre style="background: #EEE; padding: 8px">${text}</pre>`,
+    `<iframe seamless sandbox width="640" height="600" style="resize: both" srcdoc="${html.replaceAll(
+      '"',
+      '&quot;'
+    )}" />`,
+  ].join('\n\n');
+}
+
+const appBaseUrl = 'http://localhost:8000';
+const firstName = 'Baptiste';
+
+const variables = {
+  test: {
+    appBaseUrl,
+    variable: 'value',
+  },
+
+  authentication: {
+    appBaseUrl,
+    firstName,
+    authenticationUrl: appBaseUrl + '?auth-token=token',
+  },
+
+  notification: {
+    appBaseUrl,
+    firstName,
+    content: 'Content',
+    title: 'Title',
+  },
+
+  requestCommentCreated: {
+    appBaseUrl,
+    firstName,
+    request: {
+      id: 'requestId',
+      title: 'Accorder un piano',
+      author: {
+        id: 'authorId',
+        firstName: 'Maxence',
+        lastName: 'Duparc',
+      },
+    },
+    comment: {
+      id: 'commentId',
+      message:
+        "J'ai un ami qui est prof de musique, il pourra probablement t'aider. Est-ce que tu veux que je lui demande ?",
+      author: {
+        id: 'authorId',
+        firstName: 'Thibault',
+        lastName: 'Bonnet',
+      },
+    },
+  },
+};
+
+await fs.mkdir('outputs', { recursive: true });
+
+for (const [name, props] of Object.entries(variables)) {
+  await fs.writeFile(`outputs/${name}.html`, output(emails[name](props)));
+}
