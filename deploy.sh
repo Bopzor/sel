be40338b64aab60bc81e1953bf2617a199adeb87
nilscox <nils@koyeb.com>
2024-01-31 12:48:01 +0100

    chore: update the deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 repository=
 branch=
 app_name=
+app_url=
+analytics_url=
+analytics_site_id=
 
 koyeb secret create "$app_name-db-url" --value ''
 koyeb secret create "$app_name-session-secret" --value ''
@@ -8,6 +11,8 @@ koyeb secret create "$app_name-email-host" --value ''
 koyeb secret create "$app_name-email-from" --value ''
 koyeb secret create "$app_name-email-password" --value ''
 koyeb secret create "$app_name-slack-webhook-url" --value ''
+koyeb secret create "$app_name-web-push-public-key" --value ''
+koyeb secret create "$app_name-web-push-private-key" --value ''
 koyeb secret create "$app_name-geoapify-key" --value ''
 
 koyeb app create "$app_name"
@@ -17,22 +22,25 @@ koyeb service create server \
   --type web \
   --git "$repository" \
   --git-branch "$branch" \
-  --git-build-command 'pnpm exec tsc -b && pnpm run --filter @sel/server build' \
+  --git-build-command 'pnpm exec tsc -b && pnpm run --filter @sel/emails build && pnpm run --filter @sel/server build' \
   --git-run-command 'pnpm run --filter @sel/server start' \
   --instance-type micro \
   --regions fra \
   --env HOST='0.0.0.0' \
   --env PORT='3000' \
-  --env SESSION_SECRET='@$app_name-session-secret' \
+  --env SESSION_SECRET="@$app_name-session-secret" \
   --env SESSION_SECURE='true' \
-  --env APP_BASE_URL='https://selonsnous.nilscox.dev' \
-  --env DB_URL='@$app_name-db-url' \
-  --env EMAIL_HOST='ssl0.ovh.net' \
+  --env APP_BASE_URL="$app_url" \
+  --env DB_URL="@$app_name-db-url" \
+  --env EMAIL_HOST="@$app_name-email-host" \
   --env EMAIL_PORT='465' \
   --env EMAIL_SECURE='true' \
-  --env EMAIL_FROM='selons-nous@nilscox.dev' \
-  --env EMAIL_PASSWORD='@$app_name-email-password' \
-  --env EMAIL_TEMPLATES='./email-templates' \
+  --env EMAIL_FROM="@$app_name-email-from" \
+  --env EMAIL_PASSWORD="@$app_name-email-password" \
+  --env SLACK_WEBHOOK_URL="@$app_name-slack-webhook-url" \
+  --env WEB_PUSH_SUBJECT="$app_url" \
+  --env WEB_PUSH_PUBLIC_KEY="$app_name-web-push-public-key" \
+  --env WEB_PUSH_PRIVATE_KEY="$app_name-web-push-public-key" \
   --ports 3000:http \
   --routes /api:3000 \
   --checks 3000:http:/health \
@@ -47,9 +55,9 @@ koyeb service create web \
   --git-run-command 'pnpx serve -p 8080 --single apps/web/dist' \
   --instance-type nano \
   --regions fra \
-  --env VITE_ANALYTICS_URL='https://analytics.nilscox.dev' \
-  --env VITE_ANALYTICS_SITE_ID='12' \
-  --env VITE_GEOAPIFY_API_KEY=@sel-staging-geoapify-key \
+  --env VITE_ANALYTICS_URL="$analytics_url" \
+  --env VITE_ANALYTICS_SITE_ID="$analytics_site_id" \
+  --env VITE_GEOAPIFY_API_KEY="@$app_name-geoapify-key" \
   --ports 8080:http \
   --routes /:8080 \
   --checks 8080:http:/ \
