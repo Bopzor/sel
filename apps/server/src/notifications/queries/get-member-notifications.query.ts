843957fc432a958b7a4f6652ad3045f882206bef
nils <nilscox.dev@gmail.com>
2024-08-27 20:33:42 +0200

    refactor(notification): update the GetMemberNotifications query

--- apps/server/src/notifications/queries/get-member-notifications.query.ts
+++ apps/server/src/notifications/queries/get-member-notifications.query.ts
@@ -1,8 +1,10 @@
 import * as shared from '@sel/shared';
 import { injectableClass } from 'ditox';
+import { and, count, desc, eq, isNull } from 'drizzle-orm';
 
 import { QueryHandler } from '../../infrastructure/cqs/query-handler';
-import { NotificationRepository } from '../../persistence/repositories/notification/notification.repository';
+import { Database } from '../../persistence/database';
+import * as schema from '../../persistence/schema';
 import { TOKENS } from '../../tokens';
 
 export type GetMemberNotificationQuery = {
@@ -17,17 +19,40 @@ export type GetMemberNotificationQueryResult = {
 export class GetMemberNotifications
   implements QueryHandler<GetMemberNotificationQuery, GetMemberNotificationQueryResult>
 {
-  static inject = injectableClass(this, TOKENS.notificationRepository);
+  static inject = injectableClass(this, TOKENS.database);
 
-  constructor(private readonly notificationRepository: NotificationRepository) {}
+  constructor(private readonly database: Database) {}
 
   async handle({ memberId }: GetMemberNotificationQuery): Promise<GetMemberNotificationQueryResult> {
-    const unreadCount = await this.notificationRepository.query_countNotificationsForMember(memberId, false);
-    const notifications = await this.notificationRepository.query_getNotificationsForMember(memberId);
+    const [{ count: unreadCount }] = await this.database.db
+      .select({ count: count() })
+      .from(schema.notifications2)
+      .where(and(isNull(schema.notifications2.readAt), eq(schema.notifications2.memberId, memberId)));
+
+    const notifications = await this.database.db.query.notifications2.findMany({
+      where: eq(schema.notifications2.memberId, memberId),
+      orderBy: desc(schema.notifications2.date),
+    });
 
     return {
       unreadCount,
-      notifications,
+      notifications: notifications.map(this.mapNotification),
+    };
+  }
+
+  private mapNotification(
+    this: void,
+    notification: typeof schema.notifications2.$inferSelect,
+  ): shared.Notification {
+    return {
+      id: notification.id,
+      type: notification.type as shared.NotificationType,
+      // todo
+      title: '',
+      content: '',
+      date: notification.date.toISOString(),
+      read: notification.readAt !== null,
+      context: notification.context,
     };
   }
 }
