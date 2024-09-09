d4067c6229b6e460ebae490b3786b2abad2daaa8
nils <nilscox.dev@gmail.com>
2024-09-09 17:09:52 +0200

    chore(web): allow to specify the API base url

--- apps/web/src/infrastructure/config/env-config.adapter.ts
+++ apps/web/src/infrastructure/config/env-config.adapter.ts
@@ -6,11 +6,18 @@ export class EnvConfigAdapter implements ConfigPort {
   static inject = injectableClass(this);
 
   private static env: Record<string, string | undefined> = {
+    API_URL: parse(import.meta.env.VITE_API_URL),
     ANALYTICS_URL: parse(import.meta.env.VITE_ANALYTICS_URL),
     ANALYTICS_SITE_ID: parse(import.meta.env.VITE_ANALYTICS_SITE_ID),
     GEOAPIFY_API_KEY: parse(import.meta.env.VITE_GEOAPIFY_API_KEY),
     WEB_PUSH_PUBLIC_KEY: parse(import.meta.env.VITE_WEB_PUSH_PUBLIC_KEY),
   };
+
+  get api() {
+    return {
+      url: EnvConfigAdapter.env.API_URL,
+    };
+  }
 
   get analytics() {
     return {
