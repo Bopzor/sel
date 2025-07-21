60b5d2b7bf17966f77232a7b456f321bbcbb77a9
nils <nilscox.dev@gmail.com>
2025-07-21 17:14:07 +0200

    fix padding on the authentication layout

--- apps/web/src/pages/authentication/authentication.layout.tsx
+++ apps/web/src/pages/authentication/authentication.layout.tsx
@@ -22,7 +22,7 @@ export function AuthenticationLayout(props: { children?: JSX.Element }) {
       </Match>
 
       <Match when={true}>
-        <div class="absolute inset-0 col items-center justify-center">
+        <div class="absolute inset-0 col items-center justify-center p-4">
           <div class="w-full max-w-3xl overflow-hidden rounded-lg bg-neutral shadow-lg">
             <Header />
             <main class="p-4">{props.children}</main>
