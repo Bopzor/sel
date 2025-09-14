2dfc45a896eaf09c236758c8e4ac82989170cf26
Bopzor <26028196+Bopzor@users.noreply.github.com>
2025-09-14 12:19:52 +0200

    uniformize publication date display in information detail page

--- apps/web/src/pages/information/information-details.page.tsx
+++ apps/web/src/pages/information/information-details.page.tsx
@@ -1,10 +1,9 @@
-import { Information } from '@sel/shared';
 import { useParams } from '@solidjs/router';
 import { useQuery } from '@tanstack/solid-query';
 import { Show } from 'solid-js';
 
 import { apiQuery } from 'src/application/query';
-import { Card } from 'src/components/card';
+import { card } from 'src/components/card';
 import { Comments } from 'src/components/comments';
 import { MemberAvatarName } from 'src/components/member-avatar-name';
 import { Message } from 'src/components/message';
@@ -22,31 +21,31 @@ export function InformationDetailsPage() {
     <Show when={query.data} fallback={<Skeleton />}>
       {(information) => (
         <div class="col gap-8">
-          <Card title={<InformationDetailHeader information={information()} />}>
-            <h1 class="mb-4 text-3xl">{query.data?.title}</h1>
-            <Message message={query.data?.message} />
-          </Card>
+          <section>
+            <header class={card.header({ class: 'row items-end justify-between gap-2' })}>
+              <MemberAvatarName member={information().author} classes={{ root: card.title() }} />
+              <div class="text-sm text-dim">
+                <T
+                  id="date"
+                  values={{
+                    date: (
+                      <FormattedDate date={information().publishedAt} dateStyle="long" timeStyle="short" />
+                    ),
+                  }}
+                />
+              </div>
+            </header>
+
+            <div class={card.content()}>
+              <h1 class="mb-4 text-3xl">{query.data?.title}</h1>
+              <Message message={query.data?.message} />
+            </div>
+          </section>
 
           <Comments entityType="information" entityId={information().id} />
         </div>
       )}
     </Show>
-  );
-}
-
-function InformationDetailHeader(props: { information: Information }) {
-  return (
-    <div class="row items-end justify-between gap-2">
-      <MemberAvatarName member={props.information.author} />
-      <div class="text-sm text-dim">
-        <T
-          id="date"
-          values={{
-            date: <FormattedDate date={props.information.publishedAt} dateStyle="long" timeStyle="short" />,
-          }}
-        />
-      </div>
-    </div>
   );
 }
 
