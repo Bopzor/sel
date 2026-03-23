23dfc4e03421339e6961212d651302bc4221bb93
nils <nilscox.dev@gmail.com>
2026-03-23 11:45:02 +0100

    move the root error boundary to the index file

--- apps/web/src/index.tsx
+++ apps/web/src/index.tsx
@@ -1,6 +1,7 @@
 /* @refresh reload */
 import './application/side-effects';
 
+import { ErrorBoundary } from 'solid-js';
 import { render } from 'solid-js/web';
 
 // @ts-expect-error font import
@@ -12,8 +13,16 @@ import '@fontsource-variable/playpen-sans';
 import './index.css';
 
 import { App } from './app';
+import { ErrorFallback } from './components/error-boundary';
 
-render(() => <App />, document.getElementById('root') as HTMLElement);
+render(
+  () => (
+    <ErrorBoundary fallback={(error) => <ErrorFallback report error={error} />}>
+      <App />
+    </ErrorBoundary>
+  ),
+  document.getElementById('root') as HTMLElement,
+);
 
 if ('serviceWorker' in navigator) {
   void navigator.serviceWorker.register('/sw.js');
