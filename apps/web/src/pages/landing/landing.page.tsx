8634df781ae36db7d09d67fc507f23330d152542
nils <nilscox.dev@gmail.com>
2026-09-17 19:27:44 +0200

    redirect from the landing page when authenticated

--- apps/web/src/pages/landing/landing.page.tsx
+++ apps/web/src/pages/landing/landing.page.tsx
@@ -1,3 +1,9 @@
+import { useNavigate } from '@solidjs/router';
+import { createEffect, Show } from 'solid-js';
+
+import { queryAuthenticatedMember } from 'src/application/query';
+import { SpinnerFullScreen } from 'src/components/spinner';
+
 import { Hero } from './01-hero';
 import { Overview } from './02-overview';
 import { How } from './03-how';
@@ -6,12 +12,21 @@ import { Footer } from './components/footer';
 import { Header } from './components/header';
 
 export function LandingPage() {
+  const member = queryAuthenticatedMember();
+  const navigate = useNavigate();
+
+  createEffect(() => {
+    if (member.isSuccess) {
+      navigate('/home');
+    }
+  });
+
   return (
-    <>
+    <Show when={!member.isPending} fallback={<SpinnerFullScreen />}>
       <Header />
       <Main />
       <Footer />
-    </>
+    </Show>
   );
 }
 
